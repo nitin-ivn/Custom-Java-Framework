808d77d592b57786c9989144f25514b4e96eb417
Sai Krishna <ivnsainitin@gmail.com>
2025-12-20 11:23:23 +0530

    singleton implementation

--- src/main/java/io/nitin/di/ComponentScanner.java
+++ src/main/java/io/nitin/di/ComponentScanner.java
@@ -1,4 +1,50 @@
-package io.nitin.di;x
+package io.nitin.di;
+
+import io.nitin.annotations.Service;
+
+import java.io.File;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ComponentScanner {
+    private static final String packageName = "io.nitin.demo";
+
+    public static Set<Class<?>> scan() {
+        return scanPackage(packageName);
+    }
+
+    private static Set<Class<?>> scanPackage(String pkgName){
+        Set<Class<?>> set = new HashSet<>();
+
+        try{
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
+            String path = pkgName.replace('.','/');
+            URL url = classLoader.getResource(path);
+
+            if(url != null) {
+                File directory = new File(url.toURI());
+                File[] files = directory.listFiles();
+
+                if(files == null) return set;
+                for (File file : files) {
+                    if (file.isDirectory()) {
+                        set.addAll(scanPackage(pkgName + "." + file.getName()));
+                    } else if (file.getName().endsWith(".class")) {
+                        if (file.getName().contains("$")) continue;
+                        String className = pkgName + '.' + file.getName().substring(0, file.getName().length() - 6);
+                        Class<?> clazz = Class.forName(className);
+                        if (clazz.isAnnotationPresent(Service.class)) {
+                            set.add(clazz);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return set;
+    }
 }
--- src/main/java/io/nitin/di/Container.java
+++ src/main/java/io/nitin/di/Container.java
@@ -1,28 +1,51 @@
 package io.nitin.di;
 
+import io.nitin.annotations.Inject;
 import io.nitin.annotations.Service;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Container {
     private Map<Class<?>, Object> services = new HashMap<>();
+    private Set<Class<?>> annotatedClasses;
 
-    public Container(Class<?>... classes) throws Exception {
-        for (Class<?> clazz : classes) {
+    public Container() throws Exception {
+        annotatedClasses = ComponentScanner.scan();
+        for (Class<?> clazz : annotatedClasses) {
             if (clazz.isAnnotationPresent(Service.class)) {
                 services.put(clazz, clazz.getDeclaredConstructor().newInstance());
             }
         }
-//        for (Object service : services.values()) {
-//            for (Field field : service.getClass().getDeclaredFields()) {
-//                if (field.isAnnotationPresent(Inject.class)) {
-//                    field.setAccessible(true);
-//                    field.set(service, services.get(field.getType()));
-//                }
-//            }
-//        }
+
+        for (Object service : services.values()) {
+            for (Field field : service.getClass().getDeclaredFields()) {
+                if (field.isAnnotationPresent(Inject.class)) {
+                    field.setAccessible(true);
+                    Object dependency = getDependency(field.getType());
+                    if (dependency == null) {
+                        System.out.println("Will be handled by custom exceptions later");
+                        throw new Exception();
+                    }
+                    field.set(service, dependency);
+                }
+            }
+        }
+    }
+
+    private Object getDependency(Class<?> type){
+        if(services.containsKey(type)){
+            return services.get(type);
+        }
+
+        for(Class<?> clazz : services.keySet()){
+            if(type.isAssignableFrom(clazz)){
+                return services.get(clazz);
+            }
+        }
+        return null;
     }
 
     public <T> T getService(Class<T> clazz) {
